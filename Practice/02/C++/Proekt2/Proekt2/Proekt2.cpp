138ab2952505caa9f347ba09ad90ae08046067a1
Маслов Дмитрий <dmitry.forward@bk.ru>
2020-09-21 10:33:05 +0300

    С++ 1,2,3,4

---
+++ Practice/02/C++/Proekt2/Proekt2/Proekt2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int my = 3;
+	double my2 = 3.14;
+	cout << my << endl << my2;
+
+}
+
---
+++ Practice/04/C++/proekt 4/proekt 4/proekt 4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+
+    int temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    
+    int c = 6;
+    int d = 4;
+
+    c = c + d;
+    d = d - c;
+    d = -d;
+    c = c - d;
+}
