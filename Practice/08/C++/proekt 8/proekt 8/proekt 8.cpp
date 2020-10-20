1e0a65b9cd2f892bb6456048c73ec0ad015d5b55
Маслов Дмитрий <dmitry.forward@bk.ru>
2020-10-21 02:35:56 +0300

    NEW 21.10

---
+++ Practice/08/C++/proekt 8/proekt 8/proekt 8.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+#include <locale>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c;
+    char f;
+    cin >> a >> b >> c;
+
+    switch (f)
+    {
+    case '-':
+    {
+        c = a - b;
+        cout << c;
+    }
+    break;
+    case '+':
+    {
+        c = a + b;
+        cout << f;
+    }
+    break;
+    case '*':
+    {
+        c = a * c;
+        cout << f;
+    }
+    break;
+    case '/':
+        if (b == 0)
+        {
+            cout << "Некорректный ввод";
+        }
+        else
+        {
+            c = a / b;
+            cout << f;
+        }
+        break;
+    default:
+    }
+    return 0;
+}
