5b4667b6f77fdceaddbcc852133eb00c30f6a39b
Маслов Дмитрий <dmitry.forward@bk.ru>
2020-11-12 20:41:48 +0500

    23.1

---
+++ Practice/23.1/C++/PROEKT 23.1/PROEKT 23.1/PROEKT 23.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int factorial(int n) 
+{
+	if (n > 10) exit(1);
+		if (n < 0) return 1;
+		return n < 1 ? 1 : n * factorial(n - 1);
+}
+int main() 
+{
+	cout << "n: ";	
+		int n;
+		cin >> n;
+			cout << factorial(n) << endl;
+	system("pause");
+}
