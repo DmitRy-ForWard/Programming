53dbd29e8d41e1f441604b4296b2733d6af9dd9e
Маслов Дмитрий <dmitry.forward@bk.ru>
2020-12-11 19:51:51 +0500

    25-28

---
+++ Practice/27/С++/27/27/27.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <set>
+#include <iterator>
+
+using namespace std;
+
+int main() {
+    int f;
+    cin >> f;
+
+    set<int> a;
+
+    for (int i = 0; i < f; ++i) {
+        int d;
+        cin >> d;
+
+        a.insert(d);
+
+        int j = 0;
+
+        set<int> b;
+
+        for (auto it = a.cbegin(); it != a.cend() && j < 5; ++it, ++j)
+            b.insert(*it);
+
+        copy(b.crbegin(), b.crend(), ostream_iterator<int>(cout, " "));
+
+        cout << '\n';
+    }
+}
---
+++ Practice/28/C++/28/28/28.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<map>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void print_factorization(unsigned int f)
+{
+    map  <unsigned int, unsigned int> dict;
+    int div = 2;
+    while (f > 1)
+    {
+        for (unsigned int d = 2; d <= d; d++)
+        {
+            if (!(f % d))
+            {
+                f /= d;
+
+                dict[d] = dict.count(d) ? dict[d] + 1 : 1;
+                break;
+            }
+        }
+    }
+    cout << "1";
+    for (auto now : dict)
+    {
+        if (now.second > 1)
+        {
+            cout << "*" << now.first << "^" << now.second;
+        }
+        else
+        {
+            cout << "*" << now.first;
+        }
+    }
+}
+int main()
+{
+    unsigned int f;
+    cin >> f;
+    print_factorization(f);
+}
